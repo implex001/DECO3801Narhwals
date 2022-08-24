0ffdbf1c9a9ad4036b4aaf27217c37786aaa9573
Brian Stewart <brianstewarteditor@gmail.com>
2022-08-24 22:33:03 +1000

    Refactor s3 integration class and add comments

--- lib/model/s3integration.dart
+++ lib/model/s3integration.dart
@@ -8,6 +8,7 @@ import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:caravaneering/amplifyconfiguration.dart';
 
 class S3Integration {
+  // Configures the S3 bucket instance
   Future<void> configureAmplify() async {
     // Add plugins to amplify
     final AmplifyStorageS3 storage = AmplifyStorageS3();
@@ -22,7 +23,11 @@ class S3Integration {
     }
   }
 
+  // Uploads a file containing and given content
+  // Parameters: filename - the name of the file
+  //             content - the file contents
   Future<void> upload(String filename, String content) async {
+    // Get the phone temp direction and create the file
     final tempDir = await getTemporaryDirectory();
     final exampleFile = File('${tempDir.path}/$filename.json')
       ..createSync()
@@ -34,7 +39,7 @@ class S3Integration {
           local: exampleFile,
           key: filename,
           onProgress: (progress) {
-            print('Fraction completed: ${progress.getFractionCompleted()}');
+            print('Progress: ${progress.getFractionCompleted()}');
           }
       );
       print('Successfully uploaded file: ${result.key}');
@@ -43,6 +48,7 @@ class S3Integration {
     }
   }
 
+  // Deletes the file with the filename specified
   Future<void> deleteFile(String filename) async {
     try {
       final result = await Amplify.Storage.remove(key: filename);
@@ -52,6 +58,7 @@ class S3Integration {
     }
   }
 
+  // Checks whether the file exists in the S3 bucket
   Future<bool> isFileExist(String filename) async {
     List<StorageItem> listResults = await listItems();
     for (StorageItem item in listResults) {
@@ -62,6 +69,7 @@ class S3Integration {
     return false;
   }
 
+  // Lists all the items in the S3 buckets
   Future<List<StorageItem>> listItems() async {
     List<StorageItem> listItems = <StorageItem>[];
     try {
@@ -75,23 +83,26 @@ class S3Integration {
     return listItems;
   }
 
+  // Downloads a file from the S3 bucket
+  // Returns the contents of the file as a string
   Future<String> downloadFile(String filename) async {
+    // Get the phone document directory and create a file
     final documentsDir = await getApplicationDocumentsDirectory();
     final filepath = '${documentsDir.path}/$filename.txt';
     final file = File(filepath);
 
+    // Download the file
     try {
       final result = await Amplify.Storage.downloadFile(
         key: filename,
         local: file,
         onProgress: (progress) {
-          print('Fraction completed: ${progress.getFractionCompleted()}');
+          print('Progress: ${progress.getFractionCompleted()}');
         },
       );
+
+      // Read the file as a string and return the contents
       final contents = result.file.readAsStringSync();
-      // Or you can reference the file that is created above
-      // final contents = file.readAsStringSync();
-      print('Downloaded contents: $contents');
       return contents;
     } on StorageException catch (e) {
       print('Error downloading file: $e');
