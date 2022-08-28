a3c8e28e5b8638682638845a8c6fad70f818d597
implex001 <joseph.goh@uqconnect.edu.au>
2022-08-28 16:42:33 +1000

    Linked idle step tracking to save and overlays

--- lib/games/caravan_game.dart
+++ lib/games/caravan_game.dart
@@ -1,7 +1,13 @@
-import 'package:flame/components.dart';
+import 'dart:async';
+
+import 'package:caravaneering/model/save_model.dart';
+import 'package:caravaneering/model/step_tracker.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/extensions.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 
 class CaravanGame extends FlameGame with HorizontalDragDetector {
@@ -10,37 +16,72 @@ class CaravanGame extends FlameGame with HorizontalDragDetector {
     Caravan Game
   ''';
 
-  // Do not do what this is doing and put global variables
-  // This is a placeholder to be removed
-  // We would ideally have a seperate class for each type of object
-  // and add each to a list to instantiate
-  static const speed = 2000.0;
-  final _size = Vector2.all(50);
+  late StepTracker stepTracker;
+  SaveModel? save;
 
-  Vector2 position = Vector2.all(100);
-  Vector2? target;
-
-  late Sprite T1;
-  late Image _image;
+  int backgroundSteps = 0;
 
   @override
   Future<void>? onLoad() async{
-    _image = await images.load('placeholders/T1.png');
-    Sprite T1 = Sprite(_image);
-    final player = SpriteComponent(
-      sprite: T1,
-      size: _size,
-      position: Vector2(100,200)
-    );
-    add(player);
-    camera.speed = speed;
+
+    //Set orientation
+    Flame.device.setLandscape();
+    Flame.device.fullScreen();
+
+    camera.speed = 2000;
+
+  }
+
+  @override
+  void onAttach() {
+    super.onAttach();
+
+    if (save == null) {
+      // Set up save
+      save = Provider.of<SaveModel>(buildContext!);
+      save?.init().then((s) {
+        //If first save set date to yesterday
+        DateTime? lastsave = s.getLastTime();
+        lastsave ??= DateTime.now().subtract(const Duration(days: 1));
+
+        // Set up step tracking
+        stepTracker = StepTracker();
+        stepTracker.getBackgroundStepData(
+            lastsave, DateTime.now()).then(
+                (steps) {
+                  if (steps == null) {
+                    return;
+                  }
+                  backgroundSteps = steps;
+                  if (backgroundSteps != 0) {
+                    overlays.add("StepUpdate");
+                    Timer(
+                        const Duration(seconds: 5),
+                            () => overlays.remove("StepUpdate"));
+                    s.addCoins(backgroundSteps);
+                    s.saveState(force: true);
+                  }
+            }
+        );
+        stepTracker.getStepStream().listen((event) {
+          s.addCoins(1);
+        });
+        s.startAutoSave();
+      });
+    }
+
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color(0xFFFFFFFF);
   }
 
   @override
   void onHorizontalDragUpdate(DragUpdateInfo info) {
     camera.translateBy(-info.delta.game * 3);
   }
-
+  
   @override
   void render(Canvas canvas) {
     super.render(canvas);
