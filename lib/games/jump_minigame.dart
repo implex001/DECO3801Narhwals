09aed99da97b82c81a532430846eb14cb695a5ce
implex001 <joseph.goh@uqconnect.edu.au>
2022-09-04 12:44:33 +1000

    Added jumping minigame logic files

---
+++ lib/games/jump_minigame.dart
@@ -0,0 +1,25 @@
+import 'package:caravaneering/games/minigame.dart';
+
+class JumpMiniGame extends MiniGame{
+
+  @override
+  void pause() {
+    // TODO: implement pause
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  void stop() {
+    // TODO: implement stop
+  }
+
+
+  void triggerEncounter() {
+
+  }
+
+}
---
+++ lib/games/minigame.dart
@@ -0,0 +1,11 @@
+abstract class MiniGame {
+
+  // Starts the game
+  void start();
+
+  // Pauses the game
+  void pause();
+
+  // Stops and exits the game
+  void stop();
+}
