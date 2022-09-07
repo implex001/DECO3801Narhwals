338fcf730f84dac2ccf0ce114d2a7e1f896bd03d
nhu-phan <nhu.phan@uq.edu.au>
2022-09-07 12:50:33 +1000

    added navigator to the buttons on tap with placeholder routes

--- lib/views/overlays/navbar_overlay.dart
+++ lib/views/overlays/navbar_overlay.dart
@@ -18,30 +18,46 @@ class NavbarLeftOverlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-      Image.asset(
-        'assets/images/UI/Menu.png',
-        fit: BoxFit.contain,
-        height: 30,
-        //width: MediaQuery.of(context).size.width * 0.14,
-      ),
-      Image.asset(
-        'assets/images/UI/Shop.png',
-        //width: MediaQuery.of(context).size.width * 0.14,
-        height: 30,
-        fit: BoxFit.contain,
-      ),
-      Image.asset(
-        'assets/images/UI/Skills.png',
-        //width: MediaQuery.of(context).size.width * 0.14,
-        height: 30,
-        fit: BoxFit.contain,
-      ),
-      Image.asset(
-        'assets/images/UI/Caravan.png',
-        //width: MediaQuery.of(context).size.width * 0.14,
-        height: 30,
-        fit: BoxFit.contain,
-      ),
+      GestureDetector(
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/caravan", (route) => false); // placeholder route
+          },
+          child: Image.asset(
+            'assets/images/UI/Menu.png',
+            fit: BoxFit.contain,
+            height: 30,
+          )),
+      GestureDetector(
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/shop", (route) => false);
+          },
+          child: Image.asset(
+            'assets/images/UI/Shop.png',
+            fit: BoxFit.contain,
+            height: 30,
+          )),
+      GestureDetector(
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/caravan", (route) => false); // placeholder route
+          },
+          child: Image.asset(
+            'assets/images/UI/Skills.png',
+            fit: BoxFit.contain,
+            height: 30,
+          )),
+      GestureDetector(
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/caravan", (route) => false);
+          },
+          child: Image.asset(
+            'assets/images/UI/Caravan.png',
+            fit: BoxFit.contain,
+            height: 30,
+          )),
     ]);
   }
 }
@@ -99,18 +115,26 @@ class NavbarBottomOverlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      Image.asset(
-        'assets/images/UI/Minigames.png',
-        fit: BoxFit.contain,
-        height: 30,
-        //width: MediaQuery.of(context).size.width * 0.14,
-      ),
-      Image.asset(
-        'assets/images/UI/Story.png',
-        fit: BoxFit.contain,
-        height: 30,
-        //width: MediaQuery.of(context).size.width * 0.14,
-      ),
+      GestureDetector(
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/caravan", (route) => false);
+          },
+          child: Image.asset(
+            'assets/images/UI/Minigames.png',
+            fit: BoxFit.contain,
+            height: 30,
+          )),
+      GestureDetector(
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/caravan", (route) => false);
+          },
+          child: Image.asset(
+            'assets/images/UI/Story.png',
+            fit: BoxFit.contain,
+            height: 30,
+          )),
     ]);
   }
 }
